f1a704b5902ca007321ce9c2e524b3489387d2ee
Raphael Lima <phaelslima@gmail.com>
2022-04-13 20:48:47 -0300

    feat: add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+import { Box, Flex, Icon, IconButton, Image } from '@chakra-ui/react'
+import { IoIosArrowBack } from 'react-icons/io'
+
+export function Header() {
+  const { asPath } = useRouter()
+
+  return (
+    <Box>
+      <Box position="relative" w="100%" maxW="1160" h={[50, 100]} mx="auto">
+        <Flex as="header" justify="center" align="center" w="100%" h="100%">
+          {asPath !== '/' && (
+            <Link href="/" passHref>
+              <IconButton position="absolute" left={0} aria-label="Voltar para Home" icon={<Icon as={IoIosArrowBack} fontSize={[16, 20]} />}   />
+            </Link>
+          )}
+          <Image src='/logo.svg' alt="Logo do site" w={[82, 'auto']} mx="auto" />
+        </Flex>
+      </Box>
+    </Box>
+  )
+}
