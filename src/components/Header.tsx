cdcccdc557b537f94d3816155daf2c7945c08f31
Raphael Lima <phaelslima@gmail.com>
2022-04-13 23:23:43 -0300

    feat: add responsive to header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,14 +9,19 @@ export function Header() {
 
   return (
     <Box>
-      <Box position="relative" w="100%" maxW="1160" h={[50, 100]} mx="auto">
+      <Box position="relative" w="100%" maxW="1160" h={[50, 75, 75, 100]} mx="auto">
         <Flex as="header" justify="center" align="center" w="100%" h="100%">
           {asPath !== '/' && (
             <Link href="/" passHref>
-              <IconButton position="absolute" left={0} aria-label="Voltar para Home" icon={<Icon as={IoIosArrowBack} fontSize={[16, 20]} />}   />
+              <IconButton
+                position="absolute"
+                left="0"
+                aria-label="Voltar para Home"
+                icon={<Icon as={IoIosArrowBack} fontSize={[16, 20]} />}
+              />
             </Link>
           )}
-          <Image src='/logo.svg' alt="Logo do site" w={[82, 'auto']} mx="auto" />
+          <Image src='/logo.svg' alt="Logo do site" w={[82, 135, 135, 'auto']} mx="auto" />
         </Flex>
       </Box>
     </Box>
